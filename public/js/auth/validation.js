d6a4909911eda2bb44760af47249b74b985af3a0
Raimon <raimon.txollo@gmail.com>
2025-02-07 14:16:35 +0100

    Validation validated, login validated

---
+++ public/js/auth/validation.js
@@ -0,0 +1,44 @@
+export const isValidUsername = (username) => {
+  let isValid;
+
+  const regexString = /\w{5,}/g;
+  isValid = regexString.test(username);
+
+  return isValid;
+};
+
+export const isValidPassword = (password) => {
+  let isValid;
+
+  const passwordLength = password.length;
+  const passwordLengthValidation = passwordLength >= 8;
+
+  const regexNumber = /\d{1,}/g;
+  const passwordNumberValidation = regexNumber.test(password);
+
+  const regexMayus = /[A-Z]/g;
+  const passwordLetterMayus = regexMayus.test(password);
+
+  isValid =
+    (passwordLengthValidation === passwordNumberValidation) ===
+    passwordLetterMayus;
+
+  return isValid;
+};
+
+export const isSamePassword = (password1, password2) => {
+  let isSame;
+
+  isSame = password2.includes(password1);
+
+  return isSame;
+};
+
+export const isAdultAge = (age) => {
+  let isAdult;
+
+  const adultAge = 18;
+  isAdult = age >= adultAge;
+
+  return isAdult;
+};
